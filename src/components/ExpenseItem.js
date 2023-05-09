2902e7a09633844af8ae22c0c2271f479f04eb01
Arda Koç <ardaskoc@gmail.com>
2023-05-09 12:53:45 +0300

    added new expense item component.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>May 9th 2023</div>
+      <div>
+        <h2>Shopping</h2>
+        <div>$420</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
