8e51ba1fdc45444a5e02d0b57a5070fbf8c62fd6
Arda Koç <ardaskoc@gmail.com>
2023-05-16 14:52:56 +0300

    filter process is done.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,10 +6,27 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2022");
+  const [filteredYear, setFilteredYear] = useState();
 
   function filterChangeHandler(selectedYear) {
     setFilteredYear(selectedYear);
+  }
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let content = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    content = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
   }
 
   return (
@@ -19,13 +36,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {content}
       </Card>
     </div>
   );
